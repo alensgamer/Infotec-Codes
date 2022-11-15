f79e8e279e8aaf21693ee665b159a3de7ec75cb6
alensgamer_lap <alexgocugumo5@gmail.com>
2022-11-15 12:21:47 -0600

    mi lap ya no tiene  pila

--- infotec/Clase4/Ejercicio2/Figura.java
+++ infotec/Clase4/Ejercicio2/Figura.java
@@ -2,6 +2,29 @@ package Ejercicio2;
 
 public class Figura {
     private int coordenada_x, coordenada_y, ancho, alto;
-    private double perímetro, área;
-    
+    private double perímetro, area;
+    public int getCoordenada_x(){
+        return coordenada_x;
+    }
+    public int getCoordenada_y(){
+        return coordenada_y;
+    }
+    public int getAncho(){
+        return ancho;
+    }
+    public int getAlto(){
+        return alto;
+    }
+    public double getPerimetro(){
+        return perímetro;
+    }
+    public double getArea(){
+        return area;
+    }
+    public Figura(){
+
+    }
+    public void calcula_area(){
+        area = (ancho)*(alto);
+    }
 }
---
+++ infotec/Clase4/Ejercicio2/Triangulo.java
@@ -0,0 +1,9 @@
+package Ejercicio2;
+
+public class Triangulo extends Figura{
+    
+    public static void main(String[] args) {
+        Figura fig = new Figura();
+        fig.calcula_area();
+    }
+}
